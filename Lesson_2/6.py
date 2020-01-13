2b1bd5cf98da82d5c90c9efaee50bd4eb6cfbbd1
Александр Виноградов <lvar-8@ya.ru>
2020-01-13 16:21:00 +0300

    Выполненно 2.6

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -5,3 +5,20 @@
 число, чем то, что загадано. Если за 10 попыток число не отгадано,
 то вывести загаданное число.
 """
+
+
+from random import random
+
+num = int(random() * 100)
+for i in range(1, 11):
+    print(f'Попытка {i}')
+    usernum = int(input("Отгадайте число от 0 до 100: "))
+    if usernum == num:
+        print('Вы угадали!')
+        break
+    if usernum > num:
+        print(f'Загаданное число меньше {usernum}')
+    if usernum < num:
+        print(f'Загаданное число больше {usernum}')
+    if i == 10:
+        print(f'Вы проиграли! Загаданное число {num}')
