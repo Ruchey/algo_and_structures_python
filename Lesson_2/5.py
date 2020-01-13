d5e33942a9792d049261e79b4405f9fb8b17aa1e
Александр Виноградов <lvar-8@ya.ru>
2020-01-13 16:01:36 +0300

    Выполненно 2.5

--- Lesson_2/5.py
+++ Lesson_2/5.py
@@ -3,3 +3,15 @@
 под номером 32 и заканчивая 127-м включительно.
 Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 """
+
+
+def table_symb(start, end, rows):
+    for i in range(start, end, rows):
+        print("Таблица символов:")
+        for j in range(rows):
+            if (i+j) > end:
+                break
+            print(f'{i+j} - {chr(i+j)}')
+
+
+table_symb(32, 127, 10)
