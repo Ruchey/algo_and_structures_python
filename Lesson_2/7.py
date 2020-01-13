a2c40cea4d0def7c2242a52ccbd7a5228409773c
Александр Виноградов <lvar-8@ya.ru>
2020-01-13 17:10:12 +0300

    Выполненно 2.7

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,21 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+
+
+def sum_n(n):
+    if n < 1:
+        return 0
+    n = n + sum_n(n - 1)
+    return n
+
+
+def formula(n):
+    return n * (n + 1) / 2
+
+
+n = int(input("Введите число: "))
+sumn = sum_n(n)
+forml = formula(n)
+print(f'Сумма чисел 1+2+...+n = {sumn}')
+print(f'Значение формулы n(n+1)/2 = {forml}')
