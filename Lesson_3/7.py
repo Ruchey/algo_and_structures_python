7df58e7850cdf289e9f9522ef631ca4030c94bff
Александр Виноградов <lvar-8@ya.ru>
2020-01-16 16:34:26 +0300

    Выполнено 3.7

--- Lesson_3/7.py
+++ Lesson_3/7.py
@@ -3,3 +3,17 @@
 Они могут быть как равны между собой (оба являться минимальными),
  так и различаться.
 """
+
+
+from random import randint
+
+
+NUM1 = []
+for i in range(20):
+    NUM1.append(randint(1, 100))
+NUM2 = NUM1.copy()
+NUM2.sort()
+MIN1 = NUM2[0]
+MIN2 = NUM2[1]
+print(NUM2)
+print(MIN1, MIN2)
