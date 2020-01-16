e5f0ccf8ab5d2be16036dda2b5fb47a1b7d74a1a
Александр Виноградов <lvar-8@ya.ru>
2020-01-16 15:07:02 +0300

    Выполнено 3.4

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,28 @@
 # 4.	Определить, какое число в массиве встречается чаще всего.
+
+
+from random import randint
+
+
+NUM1 = []
+for i in range(20):
+    NUM1.append(randint(1, 20))
+NUM2 = NUM1.copy()
+NUM2.sort()
+CNT = 0
+NUM = NUM2[0]
+MAX_CNT = 0
+NUM_MAX_CNT = 0
+for i in NUM2:
+    if NUM == i:
+        CNT += 1
+    else:
+        CNT = 1
+    if CNT > MAX_CNT:
+        MAX_CNT = CNT
+        NUM_MAX_CNT = i
+    NUM = i
+
+print(NUM1)
+print(NUM2)
+print(NUM_MAX_CNT, MAX_CNT)
