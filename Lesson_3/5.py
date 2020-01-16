ae349f0d3f96223e5f4131760e03d9cec100f5ac
Александр Виноградов <lvar-8@ya.ru>
2020-01-16 15:43:21 +0300

    Выполнено 3.5

--- Lesson_3/5.py
+++ Lesson_3/5.py
@@ -1,2 +1,25 @@
 #5.	В массиве найти максимальный отрицательный элемент.
 # Вывести на экран его значение и позицию (индекс) в массиве.
+
+
+from random import randint
+
+
+NUM1 = []
+for i in range(20):
+    NUM1.append(randint(-10, 10))
+print(NUM1)
+NUM2 = NUM1.copy()
+NUM2.sort()
+MAX_NEG_NUM = None
+MAX_NEG_INDEX = None
+for i in range(len(NUM2)):
+    if NUM2[i] >= 0 and i > 0:
+        MAX_NEG_NUM = NUM2[i-1]
+        MAX_NEG_INDEX = i-1
+        break
+    else:
+        MAX_NEG_NUM = None
+        MAX_NEG_INDEX = None
+print(NUM2)
+print(f'Число {MAX_NEG_NUM} с индексом {MAX_NEG_INDEX}')
