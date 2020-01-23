0c4a3ea40299db4455fdc1e4566f1e32deac437d
Александр Виноградов <lvar-8@ya.ru>
2020-01-23 14:49:14 +0300

    Выполнил 5.1

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,27 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+
+COUNT = int(input('Количество предприятий: '))
+ENTERPRISE = {}
+AVERAGE_PROFIT_EN = {}
+
+while COUNT:
+    name = input('Название предприятия: ')
+    profits = list(map(int, input('Поквартальная прибыль '
+                   'I II III IV (через пробел): ').split()))
+    ENTERPRISE[name] = profits
+    AVERAGE_PROFIT_EN[name] = sum(profits)
+    COUNT -= 1
+
+EN_MAX_PROFIT = max(AVERAGE_PROFIT_EN.items(), key=lambda x: x[1])[0]
+EN_MIN_PROFIT = min(AVERAGE_PROFIT_EN.items(), key=lambda x: x[1])[0]
+
+print(f'Список предприятий: {ENTERPRISE}')
+print(f'Средняя годовая прибыль по предприятиям: {AVERAGE_PROFIT_EN}')
+print('')
+print(f'Максимальная средняя прибыль у предприятия '
+      f'{EN_MAX_PROFIT} = {AVERAGE_PROFIT_EN[EN_MAX_PROFIT]}')
+print(f'Минимальная средняя прибыль у предприятия '
+      f'{EN_MIN_PROFIT} = {AVERAGE_PROFIT_EN[EN_MIN_PROFIT]}')
