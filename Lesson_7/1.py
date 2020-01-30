bcf9ddb29703d7288ae92dc95331b22bf46ad38c
Александр Виноградов <lvar-8@ya.ru>
2020-01-30 14:07:03 +0300

    Выполнил 7.1

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,33 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+
+import timeit
+import random
+
+
+def bubble_sort(orig_list):
+    n = 1
+    while n < len(orig_list):
+        for i in range(len(orig_list)-n):
+            if orig_list[i] > orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+        n += 1
+    return orig_list
+
+
+def bubble_sort_revers(orig_list):
+    n = 1
+    while n < len(orig_list):
+        for i in range(len(orig_list)-n):
+            if orig_list[i] < orig_list[i+1]:
+                orig_list[i], orig_list[i+1] = orig_list[i+1], orig_list[i]
+        n += 1
+    return orig_list
+
+
+orig_list = [random.randint(-100, 99) for _ in range(10)]
+print(f'Оригинальный массив: {orig_list}')
+print(f'Сортировка по возрастанию: {bubble_sort(orig_list)}')
+print(f'Сортировка по убыванию: {bubble_sort_revers(orig_list)}')
